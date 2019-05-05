8434717744c9ec2fbe9295f0a880037d07f3f65f
Mirroriced <Mirroriced@192.168.0.23>
2019-05-05 17:42:16 +0200

    v1

---
+++ Boletin31_2/src/Boletin31_2/Masajista.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Boletin31_2;
+
+/**
+ *
+ * @author Mirroriced
+ */
+public class Masajista extends SeleccionFutbol {
+
+    private String titulacion;
+    private int anhosExperiencia;
+
+    public Masajista(String titulacion, int anhosExperiencia, int id, String nombre, String apellidos, int edad) {
+        super(id, nombre, apellidos, edad);
+        this.titulacion = titulacion;
+        this.anhosExperiencia = anhosExperiencia;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+
+    public void setAnhosExperiencia(int anhosExperiencia) {
+        this.anhosExperiencia = anhosExperiencia;
+    }
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public int getAnhosExperiencia() {
+        return anhosExperiencia;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("Se concrentra el Masajista");
+    }
+
+    @Override
+    public void viajar() {
+        System.out.println("Viaja el Masajista");
+    }
+
+    @Override
+    public void entrenar() {
+        System.out.println("Entrena el Masajista");
+    }
+
+    @Override
+    public void jugarPartido() {
+        System.out.println("Juega partido el Masajista");
+    }
+
+    public void darMasaje() {
+
+        System.out.println("Da masaje el Masajista");
+
+    }
+
+    @Override
+    public String toString() {
+        return "Masajista: "+super.toString() + ", Titulacion = " + titulacion + ", Años Experiencia=" + anhosExperiencia;
+    }
+
+}
